3b8271df6cec9b3c03f629cd8c0055fd1d23eed7
fayyazheva <fayya.salwa@ui.ac.id>
2023-09-20 13:55:44 +0700

    tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # models.Model adalah kelas dasar yang digunakan untuk mendefinisikan model dalam Django.
 # Product adalah nama model yang kamu definisikan.
 # nama, tanggal_tambah, harga, dan deskripsi adalah atribut atau field pada model. Setiap field memiliki tipe data yang sesuai seperti CharField, DateField, IntegerField, dan TextField.
@@ -8,3 +9,4 @@ class Product(models.Model):
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -5,14 +5,25 @@ from django.urls import reverse
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
 
     context = {
-        'name': 'Fayya Salwa Azheva', # Nama kamu
+        'name': request.user.username,
         'class': 'PBP B', # Kelas PBP kamu
-        'products': products
+        'products': products,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -23,11 +34,10 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
-
-    context = {'form': form}
-    return render(request, "create_product.html", context)\
 
 def show_xml(request):
     data = Product.objects.all()
@@ -48,6 +58,38 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 # from django.shortcuts import render berguna untuk mengimpor fungsi render dari modul django.shortcuts. Fungsi render digunakan untuk me-render tampilan HTML dengan menggunakan data yang diberikan.
 # def show_main(request) merupakan deklarasi fungsi show_main, yang menerima parameter request. Fungsi ini akan mengatur permintaan HTTP dan mengembalikan tampilan yang sesuai.
 
@@ -67,3 +109,18 @@ def show_json_by_id(request, id):
 # return HttpResponseRedirect(reverse('main:show_main')) digunakan untuk melakukan redirect setelah data form berhasil disimpan.
 
 # serializers digunakan untuk translate objek model menjadi format lain seperti dalam fungsi ini adalah XML.
+# form = UserCreationForm(request.POST) digunakan untuk membuat UserCreationForm baru dari yang sudah di-impor sebelumnya dengan memasukkan QueryDict berdasarkan input dari user pada request.POST.
+# form.is_valid() digunakan untuk memvalidasi isi input dari form tersebut.
+# form.save() digunakan untuk membuat dan menyimpan data dari form tersebut.
+# messages.success(request, 'Your account has been successfully created!') digunakan untuk menampilkan pesan kepada pengguna setelah melakukan suatu aksi.
+# return redirect('main:show_main') digunakan untuk melakukan redirect setelah data form berhasil disimpan.
+
+# logout(request) digunakan untuk menghapus sesi pengguna yang saat ini masuk.
+# return redirect('main:login') mengarahkan pengguna ke halaman login dalam aplikasi Django.
+
+# Kode from django.contrib.auth.decorators import login_required digunakan untuk mengharuskan pengguna masuk (login) sebelum dapat mengakses suatu halaman web.
+# login(request, user) berfungsi untuk melakukan login terlebih dahulu
+# response = HttpResponseRedirect(reverse("main:show_main")) untuk membuat response
+# `response.setcookie('last_login', str(datetime.datetime.now())) berfungsi untuk membuat _cookie last_login dan menambahkannya ke dalam response
+
+# response.delete_cookie('last_login') berfungsi untuk menghapus cookie last_login saat pengguna melakukan logout.
